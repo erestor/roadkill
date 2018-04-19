e24e27972b120445eaee0a32bbe396eae91b3d70
Chris S <chrisasmall+github@gmail.com>
2018-04-19 09:21:44 +0100

    The beginning of the new death star...UserRepository with Marteen and tests

---
+++ src/Roadkill.Core/Models/PageContent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Roadkill.Core.Models
+{
+    public class PageContent
+    {
+        public Guid Id { get; set; }
+
+        public Page Page { get; set; }
+
+        public string Text { get; set; }
+
+        public string EditedBy { get; set; }
+
+        public DateTime EditedOn { get; set; }
+
+        public int VersionNumber { get; set; }
+    }
+}
---
+++ src/Roadkill.Tests/Integration/Repositories/UserRepositoryTests.cs
@@ -0,0 +1,372 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoFixture;
+using Marten;
+using Newtonsoft.Json;
+using Roadkill.Core.Models;
+using Roadkill.Core.Repositories;
+using Xunit;
+
+// ReSharper disable PossibleMultipleEnumeration
+
+namespace Roadkill.Tests.Integration.Repositories
+{
+    public class UserRepositoryTests
+    {
+        private readonly Fixture _fixture;
+        public static IDocumentStore MartenDocumentStore { get; }
+
+        static UserRepositoryTests()
+        {
+            string connectionString = "host=localhost;port=5432;database=roadkill;username=roadkill;password=roadkill;";
+            MartenDocumentStore = CreateDocumentStore(connectionString);
+        }
+
+        public UserRepositoryTests()
+        {
+            _fixture = new Fixture();
+
+            new UserRepository(MartenDocumentStore).Wipe().GetAwaiter().GetResult();
+        }
+
+        public UserRepository CreateRepository()
+        {
+            return new UserRepository(MartenDocumentStore);
+        }
+
+        internal static IDocumentStore CreateDocumentStore(string connectionString)
+        {
+            var documentStore = DocumentStore.For(options =>
+            {
+                options.CreateDatabasesForTenants(c =>
+                {
+                    c.MaintenanceDatabase(connectionString);
+                    c.ForTenant()
+                        .CheckAgainstPgDatabase()
+                        .WithOwner("roadkill")
+                        .WithEncoding("UTF-8")
+                        .ConnectionLimit(-1)
+                        .OnDatabaseCreated(_ =>
+                        {
+                            Console.WriteLine("Postgres 'roadkill' database created");
+                        });
+                });
+
+                options.Connection(connectionString);
+                options.Schema.For<User>().Index(x => x.Id);
+            });
+
+            return documentStore;
+        }
+
+        private void AssertEquivalent(object expected, object actual)
+        {
+            string expectedJson = JsonConvert.SerializeObject(expected);
+            string actualJson = JsonConvert.SerializeObject(actual);
+
+            Assert.Equal(expectedJson, actualJson);
+        }
+
+        [Fact]
+        public async void DeleteAllUsers_should_clear_every_user()
+        {
+            // given
+            User adminUser = _fixture.Build<User>()
+                                     .With(x => x.IsAdmin, true)
+                                     .With(x => x.IsEditor, false)
+                                     .Create();
+
+            User editorUser = _fixture.Build<User>().With(x => x.IsAdmin, false).With(x => x.IsEditor, true).Create();
+
+            UserRepository repository = CreateRepository();
+            await repository.SaveOrUpdateUser(adminUser);
+            await repository.SaveOrUpdateUser(editorUser);
+
+            IEnumerable<User> admins = await repository.FindAllAdmins();
+            Assert.NotEmpty(admins);
+
+            IEnumerable<User> editors = await repository.FindAllEditors();
+            Assert.NotEmpty(editors);
+
+            // when
+            await repository.DeleteAllUsers();
+
+            // then
+            admins = await repository.FindAllAdmins();
+            Assert.Empty(admins);
+
+            editors = await repository.FindAllEditors();
+            Assert.Empty(editors);
+        }
+
+        [Fact]
+        public async void DeleteUser_should_delete_single_user()
+        {
+            // given
+            UserRepository repository = CreateRepository();
+            List<User> remainingUsers = _fixture.CreateMany<User>().ToList();
+            remainingUsers.ForEach(async u =>
+            {
+                await repository.SaveOrUpdateUser(u);
+            });
+
+            User actualUser = _fixture.Build<User>()
+                                      .With(x => x.IsAdmin, false)
+                                      .With(x => x.IsEditor, true)
+                                      .Create();
+
+            await repository.SaveOrUpdateUser(actualUser);
+
+            // when
+            await repository.DeleteUser(actualUser);
+
+            // then
+            User checkUser = await repository.GetUserById(actualUser.Id);
+            Assert.Null(checkUser);
+
+            var all = await repository.FindAllAdmins();
+            Guid userId = remainingUsers.First().Id;
+            User other = await repository.GetUserById(userId);
+            Assert.NotNull(other);
+        }
+
+        [Fact]
+        public async void FindAllEditors_should_return_editors_only()
+        {
+            // given
+            await new UserRepository(MartenDocumentStore).Wipe();
+
+            List<User> editorUsers = _fixture.Build<User>()
+                                                    .With(x => x.IsAdmin, false)
+                                                    .With(x => x.IsEditor, true)
+                                                    .CreateMany()
+                                                    .ToList();
+
+            UserRepository repository = CreateRepository();
+
+            editorUsers.ForEach(async u =>
+            {
+                await repository.SaveOrUpdateUser(u);
+            });
+
+            // when
+            IEnumerable<User> actualEditors = await repository.FindAllEditors();
+
+            // then
+            Assert.Equal(editorUsers.Count(), actualEditors.Count());
+            Assert.True(actualEditors.All(x => x.IsEditor));
+        }
+
+        [Fact]
+        public async void FindAllAdmins_should_return_admins_only()
+        {
+            // given
+            await new UserRepository(MartenDocumentStore).Wipe();
+
+            List<User> adminUsers = _fixture.Build<User>()
+                .With(x => x.IsAdmin, true)
+                .With(x => x.IsEditor, false)
+                .CreateMany()
+                .ToList();
+
+            UserRepository repository = CreateRepository();
+
+            adminUsers.ForEach(async u =>
+            {
+                await repository.SaveOrUpdateUser(u);
+            });
+
+            // when
+            IEnumerable<User> actualAdmins = await repository.FindAllAdmins();
+
+            // then
+            Assert.Equal(adminUsers.Count(), actualAdmins.Count());
+        }
+
+        [Fact]
+        public async void GetAdminById_should_return_admin_user()
+        {
+            // given
+            UserRepository repository = CreateRepository();
+
+            User expectedUser = _fixture.Build<User>()
+                                        .With(x => x.IsAdmin, true)
+                                        .With(x => x.IsEditor, false)
+                                        .Create();
+
+            await repository.SaveOrUpdateUser(expectedUser);
+
+            // when
+            User actualUser = await repository.GetAdminById(expectedUser.Id);
+
+            // then
+            Assert.NotNull(actualUser);
+            AssertEquivalent(expectedUser, actualUser);
+        }
+
+        [Fact]
+        public async void GetUserByActivationKey_should_return_user()
+        {
+            // given
+            UserRepository repository = CreateRepository();
+            User expectedUser = _fixture.Create<User>();
+            await repository.SaveOrUpdateUser(expectedUser);
+
+            // when
+            User actualUser = await repository.GetUserByActivationKey(expectedUser.ActivationKey);
+
+            // then
+            Assert.NotNull(actualUser);
+            AssertEquivalent(expectedUser, actualUser);
+        }
+
+        [Fact]
+        public async void GetEditorById_should_return_editor_only()
+        {
+            // given
+            UserRepository repository = CreateRepository();
+            User expectedUser = _fixture.Build<User>()
+                                            .With(x => x.IsAdmin, false)
+                                            .With(x => x.IsEditor, true)
+                                            .Create();
+
+            await repository.SaveOrUpdateUser(expectedUser);
+
+            // when
+            User actualUser = await repository.GetEditorById(expectedUser.Id);
+
+            // then
+            Assert.NotNull(actualUser);
+            AssertEquivalent(expectedUser, actualUser);
+        }
+
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public async void GetUserByEmail_should_return_user(bool isActivated)
+        {
+            // given
+            UserRepository repository = CreateRepository();
+            User expectedUser = _fixture.Build<User>()
+                                        .With(x => x.IsActivated, isActivated)
+                                        .Create();
+
+            await repository.SaveOrUpdateUser(expectedUser);
+
+            // when
+            User actualUser = await repository.GetUserByEmail(expectedUser.Email, isActivated);
+
+            // then
+            Assert.NotNull(actualUser);
+            AssertEquivalent(expectedUser, actualUser);
+        }
+
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public async void GetUserById_should_return_user(bool isActivated)
+        {
+            // given
+            UserRepository repository = CreateRepository();
+            User expectedUser = _fixture.Build<User>()
+                .With(x => x.IsActivated, isActivated)
+                .Create();
+
+            await repository.SaveOrUpdateUser(expectedUser);
+
+            // when
+            User actualUser = await repository.GetUserById(expectedUser.Id, isActivated);
+
+            // then
+            Assert.NotNull(actualUser);
+            AssertEquivalent(expectedUser, actualUser);
+        }
+
+        [Fact]
+        public async void GetUserByPasswordResetKey_should_return_user()
+        {
+            // given
+            UserRepository repository = CreateRepository();
+            User expectedUser = _fixture.Create<User>();
+
+            await repository.SaveOrUpdateUser(expectedUser);
+
+            // when
+            User actualUser = await repository.GetUserByPasswordResetKey(expectedUser.PasswordResetKey);
+
+            // then
+            Assert.NotNull(actualUser);
+            AssertEquivalent(expectedUser, actualUser);
+        }
+
+        [Fact]
+        public async void GetUserByUsername_should_return_user()
+        {
+            // given
+            UserRepository repository = CreateRepository();
+            User expectedUser = _fixture.Create<User>();
+
+            await repository.SaveOrUpdateUser(expectedUser);
+
+            // when
+            User actualUser = await repository.GetUserByUsername(expectedUser.Username);
+
+            // then
+            Assert.NotNull(actualUser);
+            AssertEquivalent(expectedUser, actualUser);
+        }
+
+        [Fact]
+        public async void GetUserByUsernameOrEmail_should_return_user()
+        {
+            // given
+            UserRepository repository = CreateRepository();
+            User expectedUser = _fixture.Create<User>();
+
+            await repository.SaveOrUpdateUser(expectedUser);
+
+            // when
+            User actualUser = await repository.GetUserByUsernameOrEmail(expectedUser.Username, expectedUser.Email);
+
+            // then
+            Assert.NotNull(actualUser);
+            AssertEquivalent(expectedUser, actualUser);
+        }
+
+        [Fact]
+        public async void SaveOrUpdateUser_should_create_new_user()
+        {
+            // given
+            UserRepository repository = CreateRepository();
+            User expectedUser = _fixture.Create<User>();
+
+            // when
+            await repository.SaveOrUpdateUser(expectedUser);
+
+            // then
+            User actualUser = await repository.GetUserById(expectedUser.Id);
+            Assert.NotNull(actualUser);
+        }
+
+        [Fact]
+        public async void SaveOrUpdateUser_should_update_user()
+        {
+            // given
+            UserRepository repository = CreateRepository();
+            User expectedUser = _fixture.Create<User>();
+            await repository.SaveOrUpdateUser(expectedUser);
+
+            expectedUser.Firstname = "My name";
+            expectedUser.Lastname = "A Jeff";
+
+            // when
+            await repository.SaveOrUpdateUser(expectedUser);
+
+            // then
+            User actualUser = await repository.GetUserById(expectedUser.Id);
+            Assert.NotNull(actualUser);
+        }
+    }
+}

7f4ecc8299bb9624d4925be04a4e2d1ba2634266
Chris S <chrisasmall+github@gmail.com>
2018-04-20 22:53:36 +0100

    PageRepository half done, with tests

---
+++ src/Roadkill.Core/Repositories/PageRepository.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Marten;
+using Roadkill.Core.Models;
+
+namespace Roadkill.Core.Repositories
+{
+    public interface IPageRepository
+    {
+        // AddNewPage and AddNewPageContentVersion should be altered so they don't return anything
+
+        Task<PageContent> AddNewPage(Page page, string text, string editedBy, DateTime editedOn);
+
+        Task<PageContent> AddNewPageContentVersion(Page page, string text, string editedBy, DateTime editedOn, int version);
+
+        // Returns a list of tags for all pages. Each item is a list of tags seperated by, e.g. { "tag1, tag2, tag3", "blah, blah2" }
+        Task<IEnumerable<Page>> AllPages();
+
+        Task<IEnumerable<PageContent>> AllPageContents();
+
+        Task<IEnumerable<string>> AllTags();
+
+        Task DeletePage(Page page);
+
+        //Removes a single version of page contents by its id.
+        Task<Task> DeletePageContent(PageContent pageContent);
+
+        Task DeleteAllPages();
+
+        Task<IEnumerable<Page>> FindPagesCreatedBy(string username);
+
+        Task<IEnumerable<Page>> FindPagesModifiedBy(string username);
+
+        Task<IEnumerable<Page>> FindPagesContainingTag(string tag);
+
+        IEnumerable<PageContent> FindPageContentsByPageId(int pageId);
+
+        IEnumerable<PageContent> FindPageContentsEditedBy(string username);
+
+        Task<PageContent> GetLatestPageContent(int pageId);
+
+        Task<Page> GetPageById(int id);
+
+        // Case insensitive search by page title
+        Task<Page> GetPageByTitle(string title);
+
+        Task<PageContent> GetPageContentById(Guid id);
+
+        Task<PageContent> GetPageContentByPageIdAndVersionNumber(int id, int versionNumber);
+
+        Task<Page> SaveOrUpdatePage(Page page);
+
+        // doesn't add a new version
+        Task UpdatePageContent(PageContent content);
+    }
+
+    public class PageRepository : IPageRepository
+    {
+        private readonly IDocumentStore _store;
+
+        public PageRepository(IDocumentStore store)
+        {
+            if (store == null)
+                throw new ArgumentNullException(nameof(store));
+
+            _store = store;
+        }
+
+        public async Task Wipe()
+        {
+            await DeleteAllPages();
+        }
+
+        public async Task<PageContent> AddNewPage(Page page, string text, string editedBy, DateTime editedOn)
+        {
+            using (IDocumentSession session = _store.LightweightSession())
+            {
+                session.Store(page);
+
+                var pageContent = new PageContent()
+                {
+                    Id = Guid.NewGuid(),
+                    EditedBy = editedBy,
+                    EditedOn = editedOn,
+                    Page = page,
+                    Text = text,
+                    VersionNumber = 1
+                };
+                session.Store(pageContent);
+
+                await session.SaveChangesAsync();
+                return pageContent;
+            }
+        }
+
+        public async Task<PageContent> AddNewPageContentVersion(Page page, string text, string editedBy, DateTime editedOn, int version)
+        {
+            PageContent existingVersion = await GetLatestPageContent(page.Id);
+            if (existingVersion == null)
+            {
+                return await AddNewPage(page, text, editedBy, editedOn);
+            }
+
+            using (IDocumentSession session = _store.LightweightSession())
+            {
+                int newVersionNumber = existingVersion.VersionNumber + 1;
+
+                var pageContent = new PageContent()
+                {
+                    Id = Guid.NewGuid(),
+                    EditedBy = editedBy,
+                    EditedOn = editedOn,
+                    Page = page,
+                    Text = text,
+                    VersionNumber = newVersionNumber
+                };
+                session.Store(pageContent);
+
+                await session.SaveChangesAsync();
+                return pageContent;
+            }
+        }
+
+        public async Task<IEnumerable<Page>> AllPages()
+        {
+            using (IQuerySession session = _store.QuerySession())
+            {
+                return await session
+                    .Query<Page>()
+                    .ToListAsync();
+            }
+        }
+
+        public Task<IEnumerable<PageContent>> AllPageContents()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<string>> AllTags()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeletePage(Page page)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Task> DeletePageContent(PageContent pageContent)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task DeleteAllPages()
+        {
+            _store.Advanced.Clean.DeleteDocumentsFor(typeof(Page));
+            await Task.CompletedTask;
+        }
+
+        public Task<IEnumerable<Page>> FindPagesCreatedBy(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Page>> FindPagesModifiedBy(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Page>> FindPagesContainingTag(string tag)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<PageContent> FindPageContentsByPageId(int pageId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<PageContent> FindPageContentsEditedBy(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<PageContent> GetLatestPageContent(int pageId)
+        {
+            using (IQuerySession session = _store.QuerySession())
+            {
+                return await session
+                    .Query<PageContent>()
+                    .OrderByDescending(x => x.VersionNumber)
+                    .FirstOrDefaultAsync(x => x.Page.Id == pageId);
+            }
+        }
+
+        public Task<Page> GetPageById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Page> GetPageByTitle(string title)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<PageContent> GetPageContentById(Guid id)
+        {
+            using (IQuerySession session = _store.QuerySession())
+            {
+                return await session
+                    .Query<PageContent>()
+                    .FirstOrDefaultAsync(x => x.Id == id);
+            }
+        }
+
+        public Task<PageContent> GetPageContentByPageIdAndVersionNumber(int id, int versionNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Page> SaveOrUpdatePage(Page page)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdatePageContent(PageContent content)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Roadkill.Tests/Integration/Repositories/PageRepositoryTests.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoFixture;
+using Marten;
+using Newtonsoft.Json;
+using Roadkill.Core.Models;
+using Roadkill.Core.Repositories;
+using Xunit;
+
+// ReSharper disable PossibleMultipleEnumeration
+
+namespace Roadkill.Tests.Integration.Repositories
+{
+    public class PageRepositoryTests
+    {
+        private readonly Fixture _fixture;
+        public static IDocumentStore MartenDocumentStore { get; }
+
+        static PageRepositoryTests()
+        {
+            string connectionString = "host=localhost;port=5432;database=roadkill;username=roadkill;password=roadkill;";
+            MartenDocumentStore = CreateDocumentStore(connectionString);
+        }
+
+        public PageRepositoryTests()
+        {
+            _fixture = new Fixture();
+            new PageRepository(MartenDocumentStore).Wipe().GetAwaiter().GetResult();
+        }
+
+        public PageRepository CreateRepository()
+        {
+            return new PageRepository(MartenDocumentStore);
+        }
+
+        internal static IDocumentStore CreateDocumentStore(string connectionString)
+        {
+            var documentStore = DocumentStore.For(options =>
+            {
+                options.CreateDatabasesForTenants(c =>
+                {
+                    c.MaintenanceDatabase(connectionString);
+                    c.ForTenant()
+                        .CheckAgainstPgDatabase()
+                        .WithOwner("roadkill")
+                        .WithEncoding("UTF-8")
+                        .ConnectionLimit(-1)
+                        .OnDatabaseCreated(_ =>
+                        {
+                            Console.WriteLine("Postgres 'roadkill' database created");
+                        });
+                });
+
+                options.Connection(connectionString);
+                options.Schema.For<User>().Index(x => x.Id);
+                options.Schema.For<Page>().Index(x => x.Id);
+            });
+
+            return documentStore;
+        }
+
+        private void AssertEquivalent(object expected, object actual)
+        {
+            string expectedJson = JsonConvert.SerializeObject(expected);
+            string actualJson = JsonConvert.SerializeObject(actual);
+
+            Assert.Equal(expectedJson, actualJson);
+        }
+
+        [Fact]
+        public async Task AddNewPage()
+        {
+            // given
+            PageRepository repository = CreateRepository();
+            Page expectedPage = _fixture.Create<Page>();
+            PageContent dummyContent = _fixture.Create<PageContent>();
+
+            // when
+            PageContent actualPageContent = await repository.AddNewPage(expectedPage, dummyContent.EditedBy, dummyContent.EditedBy, dummyContent.EditedOn);
+
+            // then
+            Assert.NotNull(actualPageContent);
+
+            PageContent savedContent = await repository.GetPageContentById(actualPageContent.Id);
+            Assert.NotNull(savedContent);
+            AssertEquivalent(actualPageContent, savedContent);
+        }
+
+        [Fact]
+        public async Task AddNewPageContentVersion_should_increment_version_for_existing_page()
+        {
+            // given
+            PageRepository repository = CreateRepository();
+            Page expectedPage = _fixture.Create<Page>();
+            PageContent dummyContent = _fixture.Create<PageContent>();
+
+            PageContent firstVersion = await repository.AddNewPage(expectedPage, dummyContent.EditedBy, dummyContent.EditedBy, dummyContent.EditedOn);
+
+            // when
+            PageContent secondVersion = await repository.AddNewPageContentVersion(expectedPage, "new content", dummyContent.EditedBy, dummyContent.EditedOn, 2);
+
+            // then
+            Assert.NotNull(secondVersion);
+
+            PageContent savedContent = await repository.GetPageContentById(secondVersion.Id);
+            Assert.NotNull(savedContent);
+            AssertEquivalent(secondVersion, savedContent);
+
+            Assert.Equal(2, secondVersion.VersionNumber);
+        }
+
+        [Fact]
+        public async Task AddNewPageContentVersion_should_addnewpage_when_no_content_exists()
+        {
+            // given
+            PageRepository repository = CreateRepository();
+            Page expectedPage = _fixture.Create<Page>();
+            PageContent dummyContent = _fixture.Create<PageContent>();
+
+            // when
+            PageContent actualPageContent = await repository.AddNewPageContentVersion(expectedPage, "new content", dummyContent.EditedBy, dummyContent.EditedOn, 2);
+
+            // then
+            Assert.NotNull(actualPageContent);
+
+            PageContent savedContent = await repository.GetPageContentById(actualPageContent.Id);
+            Assert.NotNull(savedContent);
+            AssertEquivalent(actualPageContent, savedContent);
+
+            Assert.Equal(1, actualPageContent.VersionNumber);
+        }
+
+        [Fact]
+        public async Task AllPages_should_return_all_pages()
+        {
+            // given
+            PageRepository repository = CreateRepository();
+
+            List<Page> pages = _fixture.CreateMany<Page>(10).ToList();
+            pages.ForEach(async page =>
+            {
+                PageContent dummyContent = _fixture.Create<PageContent>();
+                await repository.AddNewPage(page, dummyContent.EditedBy, dummyContent.EditedBy, dummyContent.EditedOn);
+            });
+            Thread.Sleep(1000);
+
+            // when
+            var actualPages = await repository.AllPages();
+
+            // then
+            Assert.Equal(pages.Count, actualPages.Count());
+        }
+
+        //public Task<IEnumerable<PageContent>> AllPageContents()
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        //public Task<IEnumerable<string>> AllTags()
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        //public Task DeletePage(Page page)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        //public Task<Task> DeletePageContent(PageContent pageContent)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        //public Task DeleteAllPages()
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        //public Task<IEnumerable<Page>> FindPagesCreatedBy(string username)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        //public Task<IEnumerable<Page>> FindPagesModifiedBy(string username)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        //public Task<IEnumerable<Page>> FindPagesContainingTag(string tag)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        //public IEnumerable<PageContent> FindPageContentsByPageId(int pageId)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        //public IEnumerable<PageContent> FindPageContentsEditedBy(string username)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        //public Task<PageContent> GetLatestPageContent(int pageId)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        //public Task<Page> GetPageById(int id)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        //public Task<Page> GetPageByTitle(string title)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        //public Task<PageContent> GetPageContentById(Guid id)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        //public Task<PageContent> GetPageContentByPageIdAndVersionNumber(int id, int versionNumber)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        //public Task<Page> SaveOrUpdatePage(Page page)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        //public Task UpdatePageContent(PageContent content)
+        //{
+        //    throw new NotImplementedException();
+        //}
+    }
+}

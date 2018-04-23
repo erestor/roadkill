e594c3b692e5075e34ecd6ad3b390b0a16f5f0bd
Chris S <chrisasmall+github@gmail.com>
2018-04-23 18:04:04 +0100

    Tests: Added some Text unit coverage

---
+++ src/Roadkill.Tests/Unit/Text/CustomTokens/CustomTokenParserTests.cs
@@ -0,0 +1,65 @@
+﻿using System.IO;
+using Roadkill.Core.Configuration;
+using Roadkill.Core.Text.CustomTokens;
+using Xunit;
+
+namespace Roadkill.Tests.Unit.Text.CustomTokens
+{
+    public class CustomTokenParserTests
+    {
+        static CustomTokenParserTests()
+        {
+            CustomTokenParser.CacheTokensFile = false;
+        }
+
+        [Fact]
+        public void should_contain_empty_list_when_tokens_file_not_found()
+        {
+            // Arrange
+            ApplicationSettings settings = new ApplicationSettings();
+            settings.CustomTokensPath = Path.Combine(Directory.GetCurrentDirectory(), "Unit", "Text", "CustomTokens", "doesntexist.xml");
+            CustomTokenParser parser = new CustomTokenParser(settings);
+
+            string expectedHtml = "@@warningbox:ENTER YOUR CONTENT HERE {{some link}}@@";
+
+            // Act
+            string actualHtml = parser.ReplaceTokensAfterParse("@@warningbox:ENTER YOUR CONTENT HERE {{some link}}@@");
+
+            // Assert
+            Assert.Equal(expectedHtml, actualHtml);
+        }
+
+        [Fact]
+        public void should_contain_empty_list_when_when_deserializing_bad_xml_file()
+        {
+            // Arrange
+            ApplicationSettings settings = new ApplicationSettings();
+            settings.CustomTokensPath = Path.Combine(Directory.GetCurrentDirectory(), "Unit", "Text", "CustomTokens", "badxml-file.json");
+            string expectedHtml = "@@warningbox:ENTER YOUR CONTENT HERE {{some link}}@@";
+
+            // Act
+            CustomTokenParser parser = new CustomTokenParser(settings);
+            string actualHtml = parser.ReplaceTokensAfterParse("@@warningbox:ENTER YOUR CONTENT HERE {{some link}}@@");
+
+            // Assert
+            Assert.Equal(expectedHtml, actualHtml);
+        }
+
+        [Fact]
+        public void warningbox_token_should_return_html_fragment()
+        {
+            // Arrange
+            ApplicationSettings settings = new ApplicationSettings();
+            settings.CustomTokensPath = Path.Combine(Directory.GetCurrentDirectory(), "Unit", "Text", "CustomTokens", "customvariables.xml");
+            CustomTokenParser parser = new CustomTokenParser(settings);
+
+            string expectedHtml = @"<div class=""alert alert-warning"">ENTER YOUR CONTENT HERE {{some link}}</div><br style=""clear:both""/>";
+
+            // Act
+            string actualHtml = parser.ReplaceTokensAfterParse("@@warningbox:ENTER YOUR CONTENT HERE {{some link}}@@");
+
+            // Assert
+            Assert.Equal(expectedHtml, actualHtml);
+        }
+    }
+}

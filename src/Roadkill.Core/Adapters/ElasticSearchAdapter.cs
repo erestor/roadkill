c9c715da8d4f9aa61b2debc3960a60ddeca171d9
Chris S <chrisasmall+github@gmail.com>
2018-07-16 23:42:42 +0100

    Elasticsearch impl and tests

--- src/Roadkill.Core/Adapters/ElasticSearchAdapter.cs
+++ src/Roadkill.Core/Adapters/ElasticSearchAdapter.cs
@@ -2,40 +2,71 @@
 using System.Linq;
 using System.Threading.Tasks;
 using Nest;
-using Page = Roadkill.Core.Models.Page;
+using Roadkill.Core.Models;
 
 namespace Roadkill.Core.Adapters
 {
-	public class ElasticSearchAdapter
+	public interface IElasticSearchAdapter
 	{
+		Task Add(SearchablePage page);
+
+		Task Update(SearchablePage page);
+
+		Task DeleteAll();
+
+		Task<IEnumerable<SearchablePage>> Find(string title);
+	}
+
+	public class ElasticSearchAdapter : IElasticSearchAdapter
+	{
+		private const string PagesIndexName = "pages";
 		private readonly IElasticClient _elasticClient;
 
 		public ElasticSearchAdapter(IElasticClient elasticClient)
 		{
 			_elasticClient = elasticClient;
-			_elasticClient.CreateIndexAsync("pages");
+			EnsureIndexesExist();
 		}
 
-		public async Task Add(Page page)
+		private void EnsureIndexesExist()
 		{
-			await _elasticClient.IndexAsync(page, idx => idx.Index("pages"));
+			if (!_elasticClient.IndexExists(PagesIndexName).Exists)
+			{
+				_elasticClient.CreateIndexAsync(PagesIndexName);
+			}
 		}
 
-		public async Task<IEnumerable<Page>> Find(string title)
+		public async Task Add(SearchablePage page)
 		{
-			SearchDescriptor<Page> searchDescriptor = CreateSearchDescriptor(title);
-			ISearchResponse<Page> response = await _elasticClient.SearchAsync<Page>(searchDescriptor);
+			await _elasticClient.IndexAsync(page, idx => idx.Index(PagesIndexName));
+		}
+
+		public async Task DeleteAll()
+		{
+			await _elasticClient.DeleteIndexAsync(PagesIndexName);
+		}
+
+		public Task Update(SearchablePage page)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public async Task<IEnumerable<SearchablePage>> Find(string title)
+		{
+			SearchDescriptor<SearchablePage> searchDescriptor = CreateSearchDescriptor(title);
+			ISearchResponse<SearchablePage> response = await _elasticClient.SearchAsync<SearchablePage>(searchDescriptor);
 
 			return response.Documents.AsEnumerable();
 		}
 
-		private static SearchDescriptor<Page> CreateSearchDescriptor(string title)
+		private static SearchDescriptor<SearchablePage> CreateSearchDescriptor(string title)
 		{
-			return new SearchDescriptor<Page>()
+			return new SearchDescriptor<SearchablePage>()
 						.From(0)
-						.Size(10)
-						.Index("pages")
-						.Query(q => q.Match(m => m.Field(f => f.Title).Query(title)));
+						.Size(20)
+						.Index(PagesIndexName)
+						.Query(q => q.Match(m => m.Field(f => f.Title)
+												  .Query(title)));
 		}
 	}
 }
--- src/Roadkill.Tests/Integration/Adapters/ElasticSearchAdapterTests.cs
+++ src/Roadkill.Tests/Integration/Adapters/ElasticSearchAdapterTests.cs
@@ -1,61 +1,125 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using AutoFixture;
 using Nest;
 using Roadkill.Core.Adapters;
+using Roadkill.Core.Models;
+using Shouldly;
 using Xunit;
 using Xunit.Abstractions;
-using Page = Roadkill.Core.Models.Page;
 
 namespace Roadkill.Tests.Integration.Adapters
 {
-	public class ElasticSearchAdapterTests
+	public class ElasticSearchAdapterTests : IClassFixture<ElasticSearchAdapterFixture>
 	{
 		private readonly Fixture _fixture;
 		private readonly ITestOutputHelper _console;
+		private readonly ElasticSearchAdapterFixture _classFixture;
 		private ElasticClient _elasticClient;
 
+		// These tests need ElasticSearch installed locally, you can do this
+		// by running the ElasticSearch Docker image:
+		//
+		// docker run -d -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:6.2.4
+		//
+		// ~~~~~~~~~~~~~
+		// More details:
+		//
 		// https://www.docker.elastic.co/
 		// https://www.elastic.co/guide/en/elasticsearch/reference/6.2/docker.html
-		// docker run -d -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:6.2.4
 		// https://github.com/elastic/elasticsearch-net
-
+		//
 		// RESTful api examples:
 		// https://github.com/sittinash/elasticsearch-postman
 		// http://{{url}}:{{port}}/pages/_search?pretty=true&q=*:*
+		// http://localhost:9200/pages/_search?pretty=true&q=*:*
 		// GET /_cat/indices?v
 
-		public ElasticSearchAdapterTests(ITestOutputHelper console)
+		public ElasticSearchAdapterTests(ITestOutputHelper console, ElasticSearchAdapterFixture classFixture)
 		{
 			_fixture = new Fixture();
 			_console = console;
+			_classFixture = classFixture;
 
 			var node = new Uri("http://localhost:9200");
 			var connectionSettings = new ConnectionSettings(node);
 			_elasticClient = new ElasticClient(connectionSettings);
+			Thread.Sleep(500);
+		}
+
+		private ElasticSearchAdapter CreateAdapter()
+		{
+			return new ElasticSearchAdapter(_elasticClient);
+		}
+
+		[Fact]
+		public async Task AddPage()
+		{
+			// given
+			ElasticSearchAdapter adapter = CreateAdapter();
+			string title = "A long example title";
+			var page = new SearchablePage() { Id = int.MaxValue, Title = title };
+			await adapter.Add(page);
+
+			// when
+			var results = await adapter.Find("A long");
+
+			// then
+			var firstResult = results.FirstOrDefault();
+			firstResult.ShouldNotBeNull();
+			firstResult.Title.ShouldBe(title);
+			firstResult.Id.ShouldBe(page.Id);
 		}
 
 		[Fact]
 		public async Task Find()
 		{
-			var adapter = new ElasticSearchAdapter(_elasticClient);
-			var results = await adapter.Find("long title");
+			// given
+			ElasticSearchAdapter adapter = CreateAdapter();
+			var page = _classFixture.TestPages.First();
+			Thread.Sleep(1000);
 
-			_console.WriteLine(results.Count().ToString());
-			_console.WriteLine(results.First().Title);
+			// when
+			IEnumerable<SearchablePage> results = await adapter.Find(page.Title);
+
+			// then
+			var firstResult = results.FirstOrDefault();
+			firstResult.ShouldNotBeNull();
+			firstResult.Id.ShouldBe(page.Id);
+			firstResult.Text.ShouldBe(page.Text);
+			firstResult.Title.ShouldBe(page.Title);
+			firstResult.Author.ShouldBe(page.Author);
+			firstResult.DateTime.ShouldBe(page.DateTime);
+		}
+	}
+
+	public class ElasticSearchAdapterFixture
+	{
+		public ElasticClient ElasticClient { get; set; }
+		public List<SearchablePage> TestPages { get; set; }
+
+		public ElasticSearchAdapterFixture()
+		{
+			var node = new Uri("http://localhost:9200");
+			var connectionSettings = new ConnectionSettings(node);
+			ElasticClient = new ElasticClient(connectionSettings);
+
+			AddDummyData(ElasticClient);
 		}
 
-		[Fact]
-		public async Task Add()
+		private void AddDummyData(ElasticClient elasticClient)
 		{
-			var pages = _fixture.CreateMany<Page>(10).ToList();
-			pages.Add(new Page() { Id = 900, Title = "Long title to search for" });
+			var fixture = new Fixture();
+			var adapter = new ElasticSearchAdapter(elasticClient);
+			adapter.DeleteAll().GetAwaiter().GetResult();
 
-			var adapter = new ElasticSearchAdapter(_elasticClient);
-			foreach (Page page in pages)
+			TestPages = fixture.CreateMany<SearchablePage>(10).ToList();
+			foreach (SearchablePage page in TestPages)
 			{
-				await adapter.Add(page);
+				adapter.Add(page).GetAwaiter().GetResult();
 			}
 		}
 	}

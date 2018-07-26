113a91b151b38df14c4c496fb1ea6a5e838e1a4b
Chris S <chrisasmall+github@gmail.com>
2018-07-26 12:36:26 +0100

    tests: cleanup of the Postgres manager

--- src/Roadkill.Tests/Integration/Repositories/DocumentStoreManager.cs
+++ src/Roadkill.Tests/Integration/Repositories/DocumentStoreManager.cs
@@ -30,13 +30,7 @@ namespace Roadkill.Tests.Integration.Repositories
 				return _documentStores[documentStoreSchemaName];
 			}
 
-			var keys = _documentStores.Select(x => x.Key);
-			string debug = string.Join(",", keys);
-			outputHelper.WriteLine("GetMartenDocumentStore: doc stores, items cached: {0}", debug);
-
-			outputHelper.WriteLine("...creating one");
 			IDocumentStore docStore = CreateDocumentStore(ConnectionString, documentStoreSchemaName, outputHelper);
-			outputHelper.WriteLine("...creation done");
 			_documentStores.AddOrUpdate(documentStoreSchemaName, docStore, (s, store) => store);
 
 			return _documentStores[documentStoreSchemaName];
@@ -46,10 +40,6 @@ namespace Roadkill.Tests.Integration.Repositories
 		{
 			try
 			{
-				//Action<StoreOptions> options = ConfigureOptions(connectionString, schemaName, outputHelper);
-				//outputHelper.WriteLine("...returning");
-				//var documentStore = DocumentStore.For(options);
-
 				StoreOptions options = ConfigureOptions(connectionString, schemaName, outputHelper);
 				var documentStore = new DocumentStore(options);
 
@@ -57,7 +47,7 @@ namespace Roadkill.Tests.Integration.Repositories
 			}
 			catch (Exception e)
 			{
-				outputHelper.WriteLine("oops:");
+				outputHelper.WriteLine("CreateDocumentStore failed:");
 				outputHelper.WriteLine(e.ToString());
 				return null;
 			}
@@ -65,40 +55,14 @@ namespace Roadkill.Tests.Integration.Repositories
 
 		private static StoreOptions ConfigureOptions(string connectionString, string schemaName, ITestOutputHelper outputHelper)
 		{
-			outputHelper.WriteLine("Configuring");
-			//if (!string.IsNullOrEmpty(schemaName))
-			schemaName = "roadkill";
-
 			var options = new StoreOptions();
-
-			//return options =>
-			//{
 			options.AutoCreateSchemaObjects = AutoCreate.All;
-			//options.CreateDatabasesForTenants(c =>
-			//{
-			//	c.MaintenanceDatabase(connectionString);
-			//	c.ForTenant(schemaName)
-			//		.CheckAgainstPgDatabase()
-			//		.WithOwner("postgres")
-			//		.WithEncoding("UTF-8")
-			//		.ConnectionLimit(-1)
-			//		.OnDatabaseCreated(_ =>
-			//		{
-			//			outputHelper.WriteLine("GetMartenDocumentStore: Postgres 'roadkill' database created");
-			//		});
-			//});
-
-			outputHelper.WriteLine("GetMartenDocumentStore: using '{0}' for schema name", schemaName);
-
 			options.DatabaseSchemaName = schemaName;
 			options.Connection(connectionString);
 			options.Schema.For<User>().Index(x => x.Id);
 			options.Schema.For<Page>().Identity(x => x.Id);
 			options.Schema.For<Page>().Index(x => x.Id);
 			options.Schema.For<PageVersion>().Index(x => x.Id);
-
-			outputHelper.WriteLine("...done");
-			//};
 
 			return options;
 		}

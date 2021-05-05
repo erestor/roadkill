d7ba96a9fd9dda1f0c7e78df7a4a0cf1cd1a1be5
Martin Klemsa <m.klemsa@gmail.com>
2021-05-05 10:53:42 +0200

    Renamed Main Page to Home

--- src/Roadkill.Core/Localization/SiteStrings.Designer.cs
+++ src/Roadkill.Core/Localization/SiteStrings.Designer.cs
@@ -19,7 +19,7 @@ namespace Roadkill.Core.Localization {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class SiteStrings {
@@ -880,7 +880,7 @@ namespace Roadkill.Core.Localization {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Main Page.
+        ///   Looks up a localized string similar to Home.
         /// </summary>
         public static string Navigation_MainPage {
             get {
